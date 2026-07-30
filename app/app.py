4ee267046c92c0b164cead4280c60e4fbbb060ec
shruti451 <pandeyshruti110@gmail.com>
2026-07-30 14:38:16 +0530

    Implement create, read, delete functionality for quests

--- app/app.py
+++ app/app.py
@@ -1,11 +1,38 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+tasks = []
+next_id = 1
 
-@app.route("/")
+@app.route("/",  methods=["GET", "POST"])
+
 def home():
-    return render_template("index.html")
+     global next_id
+     if request.method == "POST":
+
+        quest = request.form["quest"]
+
+        task = {
+    "id": next_id,
+    "quest": quest,
+    "completed": False
+      }
+
+        tasks.append(task)
+
+        next_id += 1
+
+     return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    for task in tasks:
+        if task["id"] == id:
+            tasks.remove(task)
+            break
+
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
